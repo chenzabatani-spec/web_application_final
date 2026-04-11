3ab773a4c6a7a86e777ca4f1787b6e0d8ff09d6b
chen <chenzabatani@gmail.com>
2026-04-11 17:29:52 +0300

    feat: setup production environment with HTTPS and build configs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,30 @@
 import './loadEnv';
 import initApp from './app';
 import './common/passport';
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 initApp().then((app) => {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
-    });
+    const port = process.env.PORT || 4000;
+    const env = process.env.NODE_ENV || 'development';
+
+    console.log('----------------------------------------');
+    console.log(`🌍 Current Environment: ${env.toUpperCase()}`);
+    console.log('----------------------------------------');
+
+    if (process.env.NODE_ENV !== 'production') {
+        http.createServer(app).listen(port, () => {
+            console.log(`✅ HTTP Server is running at http://localhost:${port}`);
+        });
+    } else {
+        const options = {
+            key: fs.readFileSync('ssl/client-key.pem'),
+            cert: fs.readFileSync('ssl/client-cert.pem')
+        };
+        
+        https.createServer(options, app).listen(port, () => {
+            console.log(`🔒 HTTPS Server is running on port ${port}`);
+        });
+    }
 });
